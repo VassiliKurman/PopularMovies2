bfa536ac455ff4512cc4ed19d26eb53f1461a6f2
Instructor79 <instructor79vk@googlemail.com>
2018-03-22 23:36:46 +0000

    feature: added method to JsonUtils
    - Added new method to JSonUtils to parse list of reviews from json file.

--- app/src/main/java/vkurman/popularmovies2/utils/JsonUtils.java
+++ app/src/main/java/vkurman/popularmovies2/utils/JsonUtils.java
@@ -10,6 +10,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import vkurman.popularmovies2.model.Movie;
+import vkurman.popularmovies2.model.Review;
 
 /**
  * Project Popular Movies stage 2.
@@ -22,13 +23,19 @@ public class JsonUtils {
     // Results Json name
     private static final String JSON_RESULTS = "results";
 
-    // Movie fields Json names
+    // Movie name fields Json file
     private static final String JSON_MOVIE_ID = "id";
     private static final String JSON_TITLE = "title";
     private static final String JSON_VOTE_AVERAGE = "vote_average";
     private static final String JSON_POSTER_PATH = "poster_path";
     private static final String JSON_RELEASE_DATE = "release_date";
     private static final String JSON_PLOT_SYNOPSIS = "overview";
+
+    // Review name fields from json file
+    private static final String JSON_REVIEW_ID = "id";
+    private static final String JSON_REVIEW_AUTHOR = "author";
+    private static final String JSON_REVIEW_CONTENT = "content";
+    private static final String JSON_REVIEW_URL = "url";
 
     /**
      * Fetches and returns list of movies from json string.
@@ -65,5 +72,40 @@ public class JsonUtils {
         }
 
         return movies;
+    }
+
+    /**
+     * Fetches and returns list of reviews from json string.
+     *
+     * @param json - string in json format
+     * @return List<Review>
+     */
+    public static List<Review> parseReviewsJson(String json) {
+
+        final List<Review> reviews = new ArrayList<>();
+
+        try {
+            // Parsing json string to json object
+            JSONObject jsonObject = new JSONObject(json);
+            // Getting json array results from json object
+            JSONArray resultsArray = jsonObject.optJSONArray(JSON_RESULTS);
+            if(resultsArray.length() > 0) {
+                Log.d(TAG, "Objects in json results array: " + resultsArray.length());
+                for(int i = 0; i < resultsArray.length(); i++) {
+                    JSONObject reviewJsonObject = resultsArray.optJSONObject(i);
+                    // Getting individual values from json object
+                    long id = reviewJsonObject.optLong(JSON_REVIEW_ID);
+                    String author = reviewJsonObject.optString(JSON_REVIEW_AUTHOR);
+                    String content = reviewJsonObject.optString(JSON_REVIEW_CONTENT);
+                    String url = reviewJsonObject.optString(JSON_REVIEW_URL);
+
+                    reviews.add(new Review(id, author, content, url));
+                }
+            }
+        } catch (JSONException e) {
+            Log.e(TAG, "Error parse Review Json: " + e);
+        }
+
+        return reviews;
     }
 }
